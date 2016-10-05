e416e48ff24781238844e8e677fcc54d6bf1b831
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-10-05 18:43:01 +0200

    Added python3-gdal and yaml

--- scripts/getDeps.sh
+++ scripts/getDeps.sh
@@ -30,7 +30,7 @@ else
         libexpat1-dev libfcgi-dev libgsl0-dev libpq-dev libqca2-dev libqca2-plugin-ossl \
         pyqt5-dev qttools5-dev qtpositioning5-dev libqt5svg5-dev libqt5webkit5-dev  \
         libqt5gui5 libqt5scripttools5 qtscript5-dev libqca-qt5-2-dev grass-dev \
-        libgeos-dev libgdal-dev libqt5xmlpatterns5-dev libqt5scintilla2-dev \
+        libgeos-dev libgdal-dev libqt5xmlpatterns5-dev libqt5scintilla2-dev python3-gdal python3-yaml \
         pyqt5.qsci-dev python3-pyqt5.qsci libgsl-dev txt2tags libproj-dev libqwt-qt5-dev \
         libspatialindex-dev pyqt5-dev-tools qttools5-dev-tools qt5-default python3-future \
         python3-pyqt5.qtsql python3-psycopg2 lighttpd locales pkg-config poppler-utils python3-dev \
