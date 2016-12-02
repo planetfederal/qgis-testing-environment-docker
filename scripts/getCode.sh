2ce0d048724a77ef61524c03681237993db7e8c7
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-12-02 17:30:19 +0100

    Fixed build for keychain master

--- scripts/getCode.sh
+++ scripts/getCode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# $1 = ${QGIS_REPOSITORY}
+# $2 = ${QGIS_BRANCH}
 set -e
 
 
@@ -7,7 +8,7 @@ cd /build
 git clone --depth 1 -b $2 $1
 cd QGIS
 
-if [ "$1"  != "master" ]; then
+if [ "$2"  != "master" ]; then
     cd src/plugins
     git clone  --depth 1 https://github.com/boundlessgeo/qgis-master-pass-cxxplugin.git
     mv qgis-master-pass-cxxplugin keychain_bridge
